95baa8faaa97c59e0ce9be09027b9897399ccb5e
Mikael Nyström <mikael.s.nystrom@gmail.com>
2017-02-24 18:15:46 +0100

    Rename libshared -> shared

--- test/project/d/module.mk
+++ test/project/d/module.mk
@@ -1,7 +1,7 @@
 lib := shared
 src := shared.c
-dvi := libshared.texi
-info := libshared.info
-data := libshareddata.dat
-pdf := libshared.pdf
-ps := libshared.ps
+dvi := shared.texi
+info := shared.info
+data := shared.dat
+pdf := shared.pdf
+ps := shared.ps
