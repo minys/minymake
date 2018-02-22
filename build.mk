68676a6a5be0dc42a6d956cebb671a6c8d94f2d2
Mikael Nyström <mikael.s.nystrom@gmail.com>
2018-02-22 11:11:26 -0500

    Update years in copyright

--- build.mk
+++ build.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2016-2017, Mikael Nyström
+# Copyright (c) 2016-2018, Mikael Nyström
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
